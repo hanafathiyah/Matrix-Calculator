3f0830fbf255a57e84230943dbdec070346bc773
hanafathiyah <hanafathiyah9b21@gmail.com>
2021-09-26 13:52:54 +0700

    menambahkan menu inverse

--- src/tubes/algeo/menu/menuInverse.java
+++ src/tubes/algeo/menu/menuInverse.java
@@ -1,4 +1,6 @@
 package tubes.algeo.menu;
+import tubes.algeo.lib.IO;
+import tubes.algeo.lib.type.Matriks;
 import tubes.algeo.lib.util.terminalColor;
 public class menuInverse {
     public static void menu(){
@@ -13,5 +15,21 @@ public class menuInverse {
                 "██ ██   ████   ████   ███████ ██   ██ ███████ ███████ \n" +
                 "                                                      \n" +
                 "                                                      \n"+ terminalColor.TEXT_RESET);
+        Matriks input = IO.readMatrix();
+        if (input.isSquare()) {
+            Matriks inverse = input.inverseByKofaktor();
+            System.out.println("Inverse matriks tersebut adalah");
+            for(int i = 0; i < inverse.getNRows(); i++) {
+                for(int j = 0; j < inverse.getNCols(); j++) {
+                    System.out.print(inverse.getElmt(i,j));
+                    if(j < inverse.getNCols() - 1) {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println("");
+            }
+        } else {
+            System.out.println("Matriks harus persegi.");
+        }
     }
 }
