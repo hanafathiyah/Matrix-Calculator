70a37558191c18ec5d4805e2b8a3a57ef8d38456
hanafathiyah <hanafathiyah9b21@gmail.com>
2021-09-26 11:23:04 +0700

    Menu determinan

--- src/tubes/algeo/menu/menuDeterminan.java
+++ src/tubes/algeo/menu/menuDeterminan.java
@@ -1,5 +1,7 @@
 package tubes.algeo.menu;
+import tubes.algeo.lib.type.Matriks;
 import tubes.algeo.lib.util.terminalColor;
+import java.io.Console;
 public class menuDeterminan {
     public static void menu(){
         menudeterminan();
@@ -11,7 +13,25 @@ public class menuDeterminan {
                 "██   ██ █████      ██    █████   ██████  ██ ████ ██ ██ ██ ██  ██ ███████ ██ ██  ██ \n" +
                 "██   ██ ██         ██    ██      ██   ██ ██  ██  ██ ██ ██  ██ ██ ██   ██ ██  ██ ██ \n" +
                 "██████  ███████    ██    ███████ ██   ██ ██      ██ ██ ██   ████ ██   ██ ██   ████ \n" +
-                "                                                                                   \n" + terminalColor.TEXT_RESET) ;
-
+                "                                                                                   \n" + terminalColor.TEXT_RESET);
+        bacamatriksdet();
+    }
+    public static void bacamatriksdet () {
+        int baris, kolom;
+        Matriks matriksdet = new Matriks();
+        Console con = System.console();
+        System.out.println("Masukkan ukuran matriks");
+        System.out.print("Baris: ");
+        baris = Integer.parseInt(con.readLine());
+        System.out.print("Kolom: ");
+        kolom = Integer.parseInt(con.readLine());
+        for (int i = 0; i < baris; i++) {
+            for(int j = 0; j < kolom; j++){
+                int elmt = Integer.parseInt(con.readLine());
+                matriksdet.setElmt(i,j,elmt);
+            }
+        }
+        matriksdet.determinanByReduksi();
+        // jujur saia bingung ini bacanya gimana tp saia ikhtiar dulu saja :"D
     }
 }
