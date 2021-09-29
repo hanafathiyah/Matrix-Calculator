52a5a9997688b94ba3e9fec1ebe8ecf404776630
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-09-29 21:33:34 +0700

    Memperbaiki HilberdBuilder

--- test/tc/HilbertBuilder.py
+++ test/tc/HilbertBuilder.py
@@ -4,15 +4,17 @@ loc = input("Path file: ")
 f = open(loc, "w")
 f.write(str(n))
 f.write(" ")
-f.write(str(n))
+f.write(str(n + 1))
 f.write("\n")
 
 for i in range(1, n + 1):
     for j in range(i, i+n):
         f.write(str(1/(j)))
-        
-        if j < i + n - 1:
-            f.write(" ")
-        else:
-            f.write("\n")
+        f.write(" ")
 
+    if(i == 1):
+        f.write("1\n")
+    else:
+        f.write("0\n")
+    
+    
