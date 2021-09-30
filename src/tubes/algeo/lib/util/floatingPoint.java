1ecb5abb9b6587da96937afc1b4fd36059c00cc6
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-09-30 10:46:33 +0700

    Mengganti isEqual check untuk floatingPOint dengan menggunakan epsilon dinamis

--- src/tubes/algeo/lib/util/floatingPoint.java
+++ src/tubes/algeo/lib/util/floatingPoint.java
@@ -1,12 +1,10 @@
 package tubes.algeo.lib.util;
 
-import java.math.BigDecimal;
-
 public class floatingPoint {
   private static final double EPSILON = 1e-9;
 
   public static boolean isEqual(double d1, double d2){
-    return Math.abs(d1-d2) < EPSILON;
+    return Math.abs(d1-d2) * 1e9 <= Math.max(Math.abs(d1), Math.abs(d2));
   }
 
   public static boolean isGreaterThan(double d1, double d2){
