c526024ad7315234ab977989f8e81fd1d7de95b9
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-09-30 23:21:29 +0700

    Menambahkan kasus untuk matriks yg lebih besar pada invers

--- src/tubes/algeo/lib/util/floatingPoint.java
+++ src/tubes/algeo/lib/util/floatingPoint.java
@@ -1,9 +1,12 @@
 package tubes.algeo.lib.util;
 
 public class floatingPoint {
-  private static final double EPSILON = 1e-9;
+  private static final double EPSILON = 1e-12;
 
   public static boolean isEqual(double d1, double d2){
+    if(d1 == d2)
+      return true;
+
     return Math.abs(d1-d2) <= EPSILON;
   }
 
